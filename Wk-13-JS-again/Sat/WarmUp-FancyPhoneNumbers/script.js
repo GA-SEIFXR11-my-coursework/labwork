91cd2f6c1a7f522734faf89508737628367eafb8
axwong <alexwong@tadyen.dev>
2023-05-27 10:41:28 +1000

    Wk13 Sat Warmup archaec phone numbers

---
+++ Wk-13-JS-again/Sat/WarmUp-FancyPhoneNumbers/script.js
@@ -0,0 +1,28 @@
+// [ -e script.js ] && rm script.js; tsc; mv main.js script.js; node script.js
+const KeypadChar2NumMap = {
+    "abc": 2,
+    "def": 3,
+    "ghi": 4,
+    "jkl": 5,
+    "mno": 6,
+    "pqrs": 7,
+    "tuv": 8,
+    "wxyz": 9,
+};
+function convertPhoneNumber(baseNum, word) {
+    var paddedNumber = "";
+    for (let c of word) {
+        for (let key in KeypadChar2NumMap) {
+            if (key.includes(c.toLowerCase())) {
+                paddedNumber += String(KeypadChar2NumMap[key]);
+                continue;
+            }
+        }
+    }
+    let longNum = baseNum + paddedNumber;
+    // limit to 10 digits:
+    return (longNum.slice(0, 10));
+}
+console.log("convertPhoneNumber('1300', 'FLOWERS') expect '1300356937' -> ", convertPhoneNumber('1300', 'FLOWERS'));
+console.log();
+console.log("convertPhoneNumber('1800', 'GOTJUNK') expect '1800468586' -> ", convertPhoneNumber('1800', 'GOTJUNK'));
